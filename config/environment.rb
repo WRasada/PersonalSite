7b28a2fd4eddfba8d0eb04e2b5992c9816a30ef5
Wesley <wesleyrasada@gmail.com>
2016-12-13 20:05:45 -0800

    Fixed Heroku Mailgun plugin

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
 	:address				=> ENV['MAILGUN_SMTP_SERVER'],
 	:user_name			=> ENV['MAILGUN_SMTP_LOGIN'],
 	:password				=> ENV['MAILGUN_SMTP_PASSWORD'],
-	:domain	   		  => 'protected-hollows-77014.herokuapp.com',
+	:domain	   		  => 'https://protected-hollows-77014.herokuapp.com/',
 	:authentication	=> :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
