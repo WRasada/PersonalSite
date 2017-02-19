01ad64517cbdd7ed102ee1326a3df47f84dfcbfb
Wesley <wesleyrasada@gmail.com>
2017-02-18 17:03:08 -0800

    Added Blog Page and updated styles

--- assets/js/personalSite.js
+++ assets/js/personalSite.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-    $('.navbar .navbar-default').slideDown(1500);
+          //************* Global JS *************//
+
+//Fade NavBar and Landing Page Content
+$('#homePage .navbar-default').fadeIn(1100, function(){
+  $('#myName').fadeIn(1000, function(){
+    $('hr').animate({
+      width: '340'
+    }, 1000);
+    $('#myNameContent').fadeIn(800);
+  });
 });
+
+$('#blogPage .navbar-default').fadeIn(1100);
