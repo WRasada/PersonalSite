f2e61c3edea02dd5920309a3c7d63409f1b1d42f
Wesley <wesleyrasada@gmail.com>
2017-02-18 22:33:11 -0800

    Added About Page and updated styles

--- assets/js/personalSite.js
+++ assets/js/personalSite.js
@@ -1,13 +1,39 @@
-          //************* Global JS *************//
+          //************* GLOBAL JS *************//
 
 //Fade NavBar and Landing Page Content
-$('#homePage .navbar-default').fadeIn(1100, function(){
+$('.navbar-default').fadeIn(1200, function(){
   $('#myName').fadeIn(1000, function(){
-    $('hr').animate({
-      width: '340'
+    $('#messageSeparator').animate({
+      width: '40%'
     }, 1000);
     $('#myNameContent').fadeIn(800);
   });
 });
 
-$('#blogPage .navbar-default').fadeIn(1100);
+$('.navbar-default')
+          //************* BLOG JS *************//
+
+$('#blogPage .navbar-default').fadeIn(1200, function(){
+  $('.blogContainer').fadeIn(1000);
+});
+
+$('.fa-plus').on("click", function(){
+  $('.post').slideToggle(1000);
+})
+          //************* ABOUT JS *************//
+
+$('#aboutPage .navbar-default').fadeIn(1200, function(){
+  $('.aboutHeader').slideDown(1100, function(){
+    $('.aboutContainer').fadeIn(800);
+  });
+});
+
+$('.navbar-nav li').mouseenter(function(){
+	if($(this).hasClass('active')){
+  } else {
+    $(this).addClass('active');
+    $(this).mouseleave(function(){
+      $(this).removeClass('active');
+    });
+  }
+});
